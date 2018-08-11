67cbfbd56a8d26988023284e86da8f651b7654a5
Abraham <kamela@uninorte.edu.co>
2018-08-11 11:51:24 -0500

    rebase with master and port env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 3000;
+//const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on ${port}/`);
 });
