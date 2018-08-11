94a8972f781bd6d6b6bd8bc8ef48f18dad5a73fa
Abraham <kamela@uninorte.edu.co>
2018-08-11 11:57:38 -0500

    bug index

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running on ${port}/`);
+  console.log(`Server running on ${port}`);
 });
